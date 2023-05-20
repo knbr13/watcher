757dfaa80e2dc9574b4b5affcc1f411cbf27b312
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-21 01:14:04 +0300

    pass the props to the child components

---
+++ src/components/Landing.jsx
@@ -0,0 +1,146 @@
+import { useState } from "react";
+import CodeEditorWindow from "./CodeEditorWindow";
+import LanguagesDropdown from "./LanguagesDropdown";
+import { languageOptions } from "../constants/languageOptions";
+import { defineTheme } from "../lib/defineTheme";
+import ThemesDropdown from "./ThemesDropdown";
+import axios from "axios";
+import { classnames } from "../utils/general";
+import OutputDetails from "./OutputDetails";
+import CustomInput from "./CustomInput";
+import OutputWindow from "./OutputWindow";
+
+const Landing = () => {
+  const [code, setCode] = useState("");
+  const [language, setLanguage] = useState(languageOptions[0]);
+  const [theme, setTheme] = useState("cobalt");
+  const [processing, setProcessing] = useState(false);
+  const [customInput, setCustomInput] = useState("");
+  const [outputDetails, setOutputDetails] = useState(null);
+
+  const onChange = (data) => {
+    setCode(data);
+  };
+
+  const handleCompile = () => {
+    setProcessing(true);
+    const formData = {
+      language_id: language.id,
+      source_code: window.atob(code),
+      stdin: window.atob(customInput),
+    };
+    const options = {
+      method: "POST",
+      url: import.meta.env.VITE_RAPID_API_URL,
+      params: { base64_encoded: "true", fields: "*" },
+      headers: {
+        "content-type": "application/json",
+        "Content-Type": "application/json",
+        "X-RapidAPI-Host": import.meta.env.VITE_RAPID_API_HOST,
+        "X-RapidAPI-Key": import.meta.env.VITE_RAPID_API_KEY,
+      },
+      data: formData,
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        const token = response.data.token;
+        checkStatus(token);
+      })
+      .catch((err) => {
+        let error = err.response ? err.response.data : err;
+        setProcessing(false);
+        alert(error);
+      });
+  };
+
+  const checkStatus = async (token) => {
+    const options = {
+      method: "GET",
+      url: import.meta.env.VITE_RAPID_API_URL + "/" + token,
+      params: { base64_encoded: "true", fields: "*" },
+      headers: {
+        "X-RapidAPI-Host": import.meta.env.VITE_RAPID_API_HOST,
+        "X-RapidAPI-Key": import.meta.env.VITE_RAPID_API_KEY,
+      },
+    };
+    try {
+      let response = await axios.request(options);
+      let statusId = response.data.status?.id;
+
+      if (statusId === 1 || statusId === 2) {
+        setTimeout(() => {
+          checkStatus(token);
+        }, 2000);
+        return;
+      } else {
+        setProcessing(false);
+        setOutputDetails(response.data);
+        return;
+      }
+    } catch (err) {
+      setProcessing(false);
+    }
+  };
+
+  function handleThemeChange(th) {
+    const theme = th;
+    if (["light", "vs-dark"].includes(theme.value)) {
+      setTheme(theme);
+    } else {
+      defineTheme(theme.value).then((_) => setTheme(theme));
+    }
+  }
+
+  const onSelectChange = (sl) => {
+    setLanguage(sl);
+  };
+
+  return (
+    <div>
+      <div className="h-4 w-full bg-gradient-to-r from-slate-600 to-black"></div>
+      <div className="flex flex-row">
+        <div className="px-4 py-2">
+          <LanguagesDropdown onSelectChange={onSelectChange} />
+        </div>
+        <div className="px-4 py-2">
+          <ThemesDropdown handleThemeChange={handleThemeChange} theme={theme} />
+        </div>
+      </div>
+      <div className="flex flex-row space-x-4 items-start px-4 py-4">
+        <div className="flex flex-col w-full h-full justify-start items-end">
+          <CodeEditorWindow
+            code={code}
+            onChange={onChange}
+            language={language?.value}
+            theme={theme.value}
+          />
+        </div>
+
+        <div className="right-container flex flex-shrink-0 w-[30%] flex-col">
+          <OutputWindow outputDetails={outputDetails} />
+          <div className="flex flex-col items-end">
+            <CustomInput
+              customInput={customInput}
+              setCustomInput={setCustomInput}
+            />
+            <button
+              onClick={handleCompile}
+              disabled={!code}
+              className={classnames(
+                "mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0",
+                !code ? "opacity-50" : ""
+              )}
+            >
+              {processing ? "Processing..." : "Compile and Execute"}
+            </button>
+          </div>
+          {outputDetails && <OutputDetails outputDetails={outputDetails} />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
