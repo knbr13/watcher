6b9240891b0fc602138e692c25cd3511bf59a62f
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-21 00:17:02 +0300

    implement onSelectChange function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
 import CodeEditorWindow from "./components/CodeEditorWindow";
+import LanguagesDropdown from "./components/LanguagesDropdown";
+import { languageOptions } from "../constants/languageOptions";
 
 const App = () => {
   const [code, setCode] = useState("");
+  const [language, setLanguage] = useState(languageOptions[0]);
 
   const onChange = (data) => {
     setCode(data);
   };
 
+  const onSelectChange = (sl) => {
+    setLanguage(sl);
+  };
+
   return (
     <div>
       Hello Code!
+      <LanguagesDropdown />
       <CodeEditorWindow />
     </div>
   );
