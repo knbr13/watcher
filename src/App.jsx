d169a29ef630ffbb608d539b1c6c7afd06f9309e
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-21 00:04:01 +0300

    implement onChange function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import CodeEditorWindow from "./components/CodeEditorWindow";
 
 const App = () => {
+  const [code, setCode] = useState("");
 
-  return <div>Hello Code!
-    <CodeEditorWindow />
-  </div>;
+  const onChange = (data) => {
+    setCode(data);
+  };
+
+  return (
+    <div>
+      Hello Code!
+      <CodeEditorWindow />
+    </div>
+  );
 };
 
 export default App;
