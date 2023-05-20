56ddcf461f900553f2c43246f84ff1951720296d
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-21 01:57:34 +0300

    remove H1 element 'output'

--- src/components/OutputWindow.jsx
+++ src/components/OutputWindow.jsx
@@ -32,11 +32,8 @@ const OutputWindow = ({ outputDetails }) => {
     };
     return (
       <>
-        <h1 className="font-bold text-xl bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-slate-700 mb-2">
-          Output
-        </h1>
         <div className="w-full h-56 bg-[#1e293b] rounded-md text-white font-normal text-sm overflow-y-auto">
-          {outputDetails ? <>{getOutput()}</> : null}
+          {outputDetails ? <>{getOutput()}</> : "Output: ..."}
         </div>
       </>
     );
