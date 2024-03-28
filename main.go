41bc053c017627a0b2e9e04b47ae1b6fbd6f89cf
knbr13 <abdullahalaadine63@gmail.com>
2024-03-28 19:41:50 +0200

    update main func

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
-	"strings"
 
+	"github.com/alexflint/go-arg"
 	"github.com/fsnotify/fsnotify"
 )
 
@@ -20,8 +19,8 @@ func init() {
 }
 
 func main() {
-	var path, command, events string
-	var recursive bool
+	var args args
+	arg.MustParse(&args)
 
 	wd, err := os.Getwd()
 	if err != nil {
@@ -29,83 +28,37 @@ func main() {
 		os.Exit(1)
 	}
 
-	flag.StringVar(&command, "cmd", "", "semicolon separated commands to run when new event occur")
-	flag.StringVar(&path, "path", wd, "path to the directory to watch for events on")
-	flag.StringVar(&events, "events", "all", "events to watch for (write, create, chmod, remove, rename, all)")
-	flag.BoolVar(&recursive, "r", false, "watch subdirectories recursively")
-	flag.Parse()
+	if args.Path != "" && !validPath(args.Path) {
+		fmt.Fprintf(os.Stderr, "err: %s\n", "invalid path: "+args.Path)
+		os.Exit(1)
+	}
 
-	options := validateAndParseFlags(
-		command,
-		path,
-		events,
-		recursive,
-	)
-	options.print()
-	watcher, _ := fsnotify.NewWatcher()
+	if args.Path == "" {
+		args.Path = wd
+	}
+
+	c := &CommandsFile{}
+
+	data, err := os.ReadFile(args.File)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "err: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	c.Parse(data)
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "err: %s\n", err.Error())
+		os.Exit(1)
+	}
 	defer watcher.Close()
 
-	if options.recursive {
-		addPathRecursively(options.path, watcher)
+	if args.Recursive {
+		addPathRecursively(args.Path, watcher)
 	} else {
-		watcher.Add(options.path)
+		watcher.Add(args.Path)
 	}
 
-	go watchEvents(watcher, options)
-	<-make(chan struct{})
-}
-
-// validateAndParseFlags function to validate the flag and build the watcherOptions struct
-func validateAndParseFlags(
-	commands string,
-	path string,
-	events string,
-	recursive bool,
-) (opt watcherOptions) {
-	fileInfo, err := os.Stat(path)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "err: %v%s\n", "invalid path: ", path)
-		os.Exit(1)
-	}
-	opt.path = path
-
-	if recursive && !fileInfo.IsDir() {
-		fmt.Fprintf(os.Stderr, "err: you can't use recursive flag with a file\n")
-		os.Exit(1)
-	}
-	opt.recursive = recursive
-
-	// parse events
-	if events == "all" {
-		opt.registredEvents = []fsnotify.Op{
-			fsnotify.Write,
-			fsnotify.Create,
-			fsnotify.Chmod,
-			fsnotify.Remove,
-			fsnotify.Rename,
-		}
-	} else {
-		events = strings.ToLower(events)
-		eventsList := strings.Split(events, ",")
-		for _, event := range eventsList {
-			event = strings.TrimSpace(event)
-			switch event {
-			case "write":
-				opt.registredEvents = append(opt.registredEvents, fsnotify.Write)
-			case "create":
-				opt.registredEvents = append(opt.registredEvents, fsnotify.Create)
-			case "chmod":
-				opt.registredEvents = append(opt.registredEvents, fsnotify.Chmod)
-			case "remove":
-				opt.registredEvents = append(opt.registredEvents, fsnotify.Remove)
-			case "rename":
-				opt.registredEvents = append(opt.registredEvents, fsnotify.Rename)
-			default:
-				fmt.Fprintf(os.Stderr, "err: %v%s\n", "invalid event: ", event)
-				os.Exit(1)
-			}
-		}
-	}
-	opt.commands = parseCommands(commands)
-	return
+	watchEvents(watcher, *c)
 }
